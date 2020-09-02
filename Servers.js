37fc08b0f3fe461e91bf53d7128521449bae1bd3
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-09-02 09:48:25 +0200

    Created Server.js

---
+++ Servers.js
@@ -0,0 +1,59 @@
+// default dependancies
+
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var net = require("net");
+var url = require("url");
+
+
+// default files
+
+var homeHtmlPage = require("./web-pages/html/home.html");
+var loginHtmlPage = require("./web-pages/html/login.html");
+var logoutHtmlPage = require("./web-pages/html/logout.html");
+var shopHtmlPage = require("./web-pages/html/shop.html");
+
+
+// default datas
+
+
+
+
+// Server function
+
+var Server = http.createServer(function (req, res) {
+     
+     var page = url.parse(req.res).query;
+     
+     if (page == "/home") {
+          
+          res.writeHead(200, {"Content-Type": "text/html"})
+          res.write(homeHtmlPage)
+          
+     }
+     
+     if (page == "/login") {
+          
+          res.writeHead(200, {"Content-Type": "text/html"})
+          res.write(loginHtmlPage)
+          
+     }
+     
+     if (page == "/logout") {
+          
+          res.writeHead(200, {"Content-Type": "text/html"})
+          res.write(logoutHtmlPage)
+          
+     }
+     
+     if (page == "/shop") {
+          
+          res.writeHead(200, {"Content-Type": "text/html"})
+          res.write(shopHtmlPage)
+          
+     }
+     
+});
+
+Server.listen(serverPort, serverAddress);
